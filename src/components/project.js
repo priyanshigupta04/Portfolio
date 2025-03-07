78b9b5b1b73e475dc95a26c24966647b99439838
Priyanshi <prigupta124@gmail.com>
2025-03-07 20:55:39 +0530

    updated resume is added

--- src/components/project.js
+++ src/components/project.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import calcyImage from '../assets/assets/calcyProject.png';
+import MediImage from '../assets/assets/MediRemind.jpg';
 import musicImage from '../assets/assets/musicPlay.png';
 
 
@@ -11,6 +11,16 @@ const project = () => (
 <h2>My Recent <span>Projects</span></h2>
 
 <div className="grid-container">
+<div className="card">
+      <img src={MediImage} alt="Project 2" className="card-image"/>
+      <div className="card-content">
+          <p className="card-description">The <b>Medicine Reminder</b> App is a web application built with React, HTML, and CSS to help users track medication intake with timely reminders.</p>
+          <div className="card-buttons">
+              <a href="https://github.com/priyanshigupta04/MediRemind.git" className="btn btn-github">GitHub</a>
+              <a href="https://medi-remind.vercel.app/" className="btn btn-demo">Demo</a>
+          </div>
+      </div>
+  </div>
   <div className="card">
       <img src={musicImage} alt="Project 1" className="card-image"/>
       <div className="card-content">
@@ -19,16 +29,6 @@ const project = () => (
           <div className="card-buttons">
               <a href="https://github.com/priyanshigupta04/musicPlay" className="btn btn-github">GitHub</a>
               <a href="https://musicplay-sox5.onrender.com" className="btn btn-demo">Demo</a>
-          </div>
-      </div>
-  </div>
-  <div className="card">
-      <img src={calcyImage} alt="Project 2" className="card-image"/>
-      <div className="card-content">
-          <p className="card-description">The <b>Calculator</b> project, developed using HTML, CSS, and JavaScript, provides a simple, interactive interface for basic arithmetic operations.</p>
-          <div className="card-buttons">
-              <a href="https://github.com/priyanshigupta04/Calcy" className="btn btn-github">GitHub</a>
-              <a href="#" className="btn btn-demo">Demo</a>
           </div>
       </div>
   </div>
