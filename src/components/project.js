21c6164f06661e46198803f7a1d2480583a75898
Priyanshi <prigupta124@gmail.com>
2025-07-18 21:00:27 +0530

    React project is added

--- src/components/project.js
+++ src/components/project.js
@@ -1,42 +1,91 @@
-import React from 'react';
-import '../App.css';
-import MediImage from '../assets/assets/MediRemind.jpg';
-import musicImage from '../assets/assets/musicPlay.png';
+import React from "react";
+import "../App.css";
+import MediImage from "../assets/assets/MediRemind.jpg";
+import musicImage from "../assets/assets/musicPlay.png";
+import hotelImage from "../assets/assets/HotelManagement.png";
 
 
 const project = () => (
-   <>
-   <div className="project">
+  <>
+    <div className="project">
+      <h2>
+        My Recent <span>Projects</span>
+      </h2>
 
-<h2>My Recent <span>Projects</span></h2>
-
-<div className="grid-container">
-<div className="card">
-      <img src={MediImage} alt="Project 2" className="card-image"/>
-      <div className="card-content">
-          <p className="card-description">The <b>Medicine Reminder</b> App is a web application built with React, HTML, and CSS to help users track medication intake with timely reminders.</p>
-          <div className="card-buttons">
-              <a href="https://github.com/priyanshigupta04/MediRemind.git" className="btn btn-github">GitHub</a>
-              <a href="https://medi-remind.vercel.app/" className="btn btn-demo">Demo</a>
+      <div className="grid-container">
+        <div className="card">
+          <img src={hotelImage} alt="Project 2" className="card-image" />
+          <div className="card-content">
+            <p className="card-description">
+              Built a responsive hotel booking app <b>QuickStay</b> with React and Tailwind CSS, featuring room listings and an interactive booking form.
+            </p>
+            <div className="card-buttons">
+              <a
+                href="https://github.com/priyanshigupta04/HotelManagement.git"
+                className="btn btn-github"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://hotel-management-qug4.vercel.app/"
+                className="btn btn-demo"
+              >
+                Demo
+              </a>
+            </div>
           </div>
-      </div>
-  </div>
-  <div className="card">
-      <img src={musicImage} alt="Project 1" className="card-image"/>
-      <div className="card-content">
-          <p className="card-description">This <b>Music Player</b> project, built with Python and Django, lets users listen to songs, create, and manage playlists with a user-friendly interface.
-          </p>
-          <div className="card-buttons">
-              <a href="https://github.com/priyanshigupta04/musicPlay" className="btn btn-github">GitHub</a>
-              <a href="https://musicplay-sox5.onrender.com" className="btn btn-demo">Demo</a>
+        </div>
+        <div className="card">
+          <img src={MediImage} alt="Project 2" className="card-image" />
+          <div className="card-content">
+            <p className="card-description">
+              The <b>Medicine Reminder</b> App is a web application built with
+              React, HTML, and CSS to help users track medication intake with
+              timely reminders.
+            </p>
+            <div className="card-buttons">
+              <a
+                href="https://github.com/priyanshigupta04/MediRemind.git"
+                className="btn btn-github"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://medi-remind.vercel.app/"
+                className="btn btn-demo"
+              >
+                Demo
+              </a>
+            </div>
           </div>
+        </div>
+        <div className="card">
+          <img src={musicImage} alt="Project 1" className="card-image" />
+          <div className="card-content">
+            <p className="card-description">
+              This <b>Music Player</b> project, built with Python and Django,
+              lets users listen to songs, create, and manage playlists with a
+              user-friendly interface.
+            </p>
+            <div className="card-buttons">
+              <a
+                href="https://github.com/priyanshigupta04/musicPlay"
+                className="btn btn-github"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://musicplay-sox5.onrender.com"
+                className="btn btn-demo"
+              >
+                Demo
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-  </div>
-  
-</div>
-
-</div>
-   </>
+    </div>
+  </>
 );
 
 export default project;
